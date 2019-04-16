80cdbbba14d9bf513ed9a717e26c8df91d7a6346
Kat B <katreesebarnes@gmail.com>
2019-04-16 19:26:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,6 +31,17 @@ def make_courses
   end
 end
 
+ def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
 
 # Scraper.new.get_page
